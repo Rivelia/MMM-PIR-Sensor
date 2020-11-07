fdd6f35c6d8dca37e8040dc1d086f9b36698c5ba
Rivelia <kevisor@gmail.com>
2020-11-07 14:39:32 +0100

    Added config vars powerSavingOnDelay and powerSavingOffDelay

--- MMM-PIR-Sensor.js
+++ MMM-PIR-Sensor.js
@@ -1,13 +1,13 @@
 /* global Module */
 
 /* Magic Mirror
-* Module: MMM-PIR-Sensor
-*
-* By Paul-Vincent Roll http://paulvincentroll.com
-* MIT Licensed.
-*/
+ * Module: MMM-PIR-Sensor
+ *
+ * By Paul-Vincent Roll http://paulvincentroll.com
+ * MIT Licensed.
+ */
 
-Module.register('MMM-PIR-Sensor',{
+Module.register('MMM-PIR-Sensor', {
 	requiresVersion: '2.1.0',
 	defaults: {
 		sensorPin: 22,
@@ -19,36 +19,42 @@ Module.register('MMM-PIR-Sensor',{
 		alwaysOffPin: false,
 		alwaysOffState: 1,
 		powerSaving: true,
-		powerSavingDelay: 0,
+		powerSavingOnDelay: 0,
+		powerSavingOffDelay: 0,
 		powerSavingNotification: false,
-		powerSavingMessage: "Monitor will be turn Off by PIR module",
-		presenceIndicator: "fa-bullseye",
-		presenceIndicatorColor: "red",
+		powerSavingMessage: 'Monitor will be turn Off by PIR module',
+		presenceIndicator: 'fa-bullseye',
+		presenceIndicatorColor: 'red',
 		presenceOffIndicator: null,
-		presenceOffIndicatorColor: "dimgray",
-		runSimulator: false
+		presenceOffIndicatorColor: 'dimgray',
+		runSimulator: false,
 	},
 
 	userPresence: false,
 
-	getStyles: function() {
-		return [
-			'font-awesome.css'
-		];
+	getStyles: function () {
+		return ['font-awesome.css'];
 	},
 
-	getDom: function() {
-		var wrapper = document.createElement("i");
+	getDom: function () {
+		var wrapper = document.createElement('i');
 		if (this.userPresence) {
-			if (this.config.presenceIndicator && this.config.presenceIndicatorColor) {
-				wrapper.className = "fas " + this.config.presenceIndicator;
-				wrapper.style = "color: " + this.config.presenceIndicatorColor + ";";
+			if (
+				this.config.presenceIndicator &&
+				this.config.presenceIndicatorColor
+			) {
+				wrapper.className = 'fas ' + this.config.presenceIndicator;
+				wrapper.style =
+					'color: ' + this.config.presenceIndicatorColor + ';';
 			}
-		}
-		else {
-			if (this.config.presenceOffIndicator && this.config.presenceOffIndicatorColor) {
-				wrapper.className = "fas " + this.config.presenceOffIndicator;
-				wrapper.style = "color: " + this.config.presenceOffIndicatorColor + ";";
+		} else {
+			if (
+				this.config.presenceOffIndicator &&
+				this.config.presenceOffIndicatorColor
+			) {
+				wrapper.className = 'fas ' + this.config.presenceOffIndicator;
+				wrapper.style =
+					'color: ' + this.config.presenceOffIndicatorColor + ';';
 			}
 		}
 		return wrapper;
@@ -59,23 +65,29 @@ Module.register('MMM-PIR-Sensor',{
 		if (notification === 'USER_PRESENCE') {
 			this.userPresence = payload;
 			this.sendNotification(notification, payload);
-			if  (payload === false && this.config.powerSavingNotification === true){
-				this.sendNotification("SHOW_ALERT",{type:"notification", message:this.config.powerSavingMessage});
+			if (
+				payload === false &&
+				this.config.powerSavingNotification === true
+			) {
+				this.sendNotification('SHOW_ALERT', {
+					type: 'notification',
+					message: this.config.powerSavingMessage,
+				});
 			}
 			this.updateDom();
 		} else if (notification === 'SHOW_ALERT') {
-			this.sendNotification(notification, payload)
+			this.sendNotification(notification, payload);
 		}
 	},
 
 	notificationReceived: function (notification, payload) {
 		if (notification === 'SCREEN_WAKEUP') {
-			this.sendNotification(notification, payload)
+			this.sendNotification(notification, payload);
 		}
 	},
 
 	start: function () {
 		this.sendSocketNotification('CONFIG', this.config);
 		Log.info('Starting module: ' + this.name);
-	}
+	},
 });
